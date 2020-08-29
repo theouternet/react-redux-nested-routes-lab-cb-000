e9ae3f54a6e4b9d0bfda7e307861270a23747b6a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-29 21:55:24 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ const App = (props) =>
   <Router>
     <div>
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+        <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
         <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Pets!</NavLink>
         <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Pet</NavLink>
       </div>
